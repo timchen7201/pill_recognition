7402f6ca0b486e0ddff581af1c7b5aaea4688a71
timchen7201 <33680174+timchen7201@users.noreply.github.com>
2019-07-19 10:57:43 +0800

    Update inception_blocks_v2.py

--- inception_blocks_v2.py
+++ inception_blocks_v2.py
@@ -209,7 +209,7 @@ def inception_block_3b(X):
 
     return inception
 
-def faceRecoModel(input_shape):
+def RecoModel(input_shape):
     """
     Implementation of the Inception model used for FaceNet
     
